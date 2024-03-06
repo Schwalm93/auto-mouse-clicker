7b450f83ea2a61f81e1805e914b480b75a16d710
Christopher Schwalm <79628051+Schwalm93@users.noreply.github.com>
2024-03-06 15:40:09 +0100

    Update MouseMover.java

--- MouseMover.java
+++ MouseMover.java
@@ -7,25 +7,17 @@ public class MouseMover {
     public static void main(String[] args) {
         try {
             Robot robot = new Robot();
-            // Definiere das Zeitintervall in Millisekunden (z.B. 5000 für 5 Sekunden)
-            int interval = 60000; // X Sekunden * 1000
+            // Time interval (60000 for 60 seconds)
+            int interval = 60000;
 
             while (true) {
-                // Aktuelle Position der Maus erhalten
                 Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
                 int currentX = mouseLocation.x;
                 int currentY = mouseLocation.y;
 
-                // Bewege die Maus um 1 Pixel diagonal
-                robot.mouseMove(currentX + 10, currentY + 10);
-
-                // Warte für das definierte Intervall
+                robot.mouseMove(currentX + 5, currentY + 5);
                 Thread.sleep(interval);
-
-                // Bewege die Maus zurück zur ursprünglichen Position
                 robot.mouseMove(currentX, currentY);
-
-                // Warte wieder, damit es eine kontinuierliche Bewegung gibt
                 Thread.sleep(interval);
             }
         } catch (AWTException | InterruptedException e) {
